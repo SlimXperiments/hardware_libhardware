CyanogenMod/android_hardware_libhardware

Add camera parameters for htc evo 3D. (2/2)
Use BOARD_HTC_3D_SUPPORT to enable.

Conflicts:
include/hardware/camera.h
modules/gralloc/Android.mk
modules/gralloc/gralloc.cpp

Change-Id: I23b668a7a5d88770cbc290940444f55f157630d3

include/hardware/camera.h
@@ -75,6 +75,9 @@ typedef struct preview_stream_ops {
     int (*cancel_buffer)(struct preview_stream_ops* w,
                 buffer_handle_t* buffer);
     int (*set_buffer_count)(struct preview_stream_ops* w, int count);
+#ifdef QCOM_HARDWARE
+    int (*set_3d_mode)(const struct preview_stream_ops *w, int r1, int r2, int r3);
+#endif
     int (*set_buffers_geometry)(struct preview_stream_ops* pw,
                 int w, int h, int format);
     int (*set_crop)(struct preview_stream_ops *w,

SlimXperiments/hardware_libhardware

include/hardware/camera.h
@@ -75,6 +75,9 @@ typedef struct preview_stream_ops {
     int (*cancel_buffer)(struct preview_stream_ops* w,
                 buffer_handle_t* buffer);
     int (*set_buffer_count)(struct preview_stream_ops* w, int count);
+#ifdef HTC_3D_SUPPORT
+    int (*set_3d_mode)(const struct preview_stream_ops *w, int r1, int r2, int r3);
+#endif
     int (*set_buffers_geometry)(struct preview_stream_ops* pw,
                 int w, int h, int format);
     int (*set_crop)(struct preview_stream_ops *w,